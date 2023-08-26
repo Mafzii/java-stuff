d871467b03faf9fda396e77c4b562689629f850e
Mafzii <23100040@lums.edu.pk>
2023-08-26 15:10:19 +0500

    JAVA: square sum

---
+++ classes-n-objects/square_sum.java
@@ -0,0 +1,11 @@
+// Square Sum
+class SquareSum {
+
+    // Method to sum the squares of three numbers
+    double squareSum(double num1, double num2, double num3) {
+      double sum = num1*num1 + num2*num2 + num3*num3;
+  
+      return sum;
+    }
+    
+  }
