d33102878e57e016b566a8a81e277a7fe6ae93ef
Mafzii <23100040@lums.edu.pk>
2023-08-30 14:28:30 +0500

    JAVA: data hiding complete

---
+++ data-hiding/getter_setter.java
@@ -0,0 +1,23 @@
+// Student Class
+class Student {
+
+  private String name;
+  private String rollNumber;
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setRollNumber(String rollNumber) {
+    this.rollNumber = rollNumber;
+  }
+
+  public String getRollNumber() {
+    return this.rollNumber;
+  }
+
+}
